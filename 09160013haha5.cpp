6c889f418519c7188f6daa08b22d690b0652a090
williamloufighter <79677166+williamloufighter@users.noreply.github.com>
2021-03-19 11:43:12 +0800

    Add files via upload

---
+++ 09160013haha5.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct POINT{
+    float x,y,z;
+}a;
+struct POINT point[5]={{0,0,0},{1,0,0},{0,1,0},{0,0,1},{1,1,1}};
+
+int main()
+{
+    struct POINT*p=&point[0];
+    printf("%.2f %.2f %.2f\n",p->x,p->y,p->z);
+
+    p++;
+    printf("%.2f %.2f %.2f\n",p->x,p->y,p->z);
+
+    p++;
+    printf("%.2f %.2f %.2f\n",p->x,p->y,p->z);
+
+}
